28cdb15be3c8a09dbd01f2b021161d525a39f2d3
Linsbeiky <hentmast227@gmail.com>
2024-10-11 19:30:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-name = 'Топинамбур'
-print(name[0],name[9],name[5:],name[::-1],name[1::2], sep = '\n')
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for num in numbers:
+    if num < 2:
+        not_primes.append(num)
+        continue
+
+    is_prime = True
+    for divisor in range(2, int(num ** 0.5) + 1):
+        if num % divisor == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+
+print(primes, not_primes, sep = '\n')
